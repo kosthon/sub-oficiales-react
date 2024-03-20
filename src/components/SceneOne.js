86a42a7410a485a44e7ad24b1cfb8282e22476f8
IngDanielReinaG <122120756+IngDanielReinaG@users.noreply.github.com>
2024-03-20 16:08:08 -0500

    Posicion botones 4 escenas

--- src/components/SceneOne.js
+++ src/components/SceneOne.js
@@ -1,26 +1,31 @@
 // SceneOne.js
-import React from 'react';
-import {Sky, Text, Entity} from 'react-aframe-ar';
-import Button from './Button';
+import React from "react";
+import { Sky, Text, Entity } from "react-aframe-ar";
+import Button from "./Button";
 
-export default function SceneOne({visible, changeScene}) {
-	return (
-		<Entity id='SceneOne' visible={visible}>
-			<Sky src='/img/entrance.jpg' rotation='0 -130 0' />
+export default function SceneOne({ visible, changeScene }) {
+  return (
+    <Entity id="SceneOne" visible={visible}>
+      <Sky src="/img/entrance.jpg" rotation="0 -130 0" />
 
-			<Entity
-				geometry='primitive: plane; width: 1; height: 0.5;'
-				material='color: #FFFFFF;'
-				position='-5 1.5 -3.7'
-				rotation='0 40.34 0'
-				className='clickable'
-				scale='1.13 1 0.04'
-				events={{click: () => changeScene('SceneTwo')}}
-			>
-				<Text value='Check In' align='center' position='0 0 0.01' color='#000000' />
-			</Entity>
+      <Entity
+        geometry="primitive: plane; width: 1; height: 0.5;"
+        material="color: #FFFFFF;"
+        position="-4.5 2.5 -3.7"
+        rotation="0.4102377813136694 40.611821476667885 2.7347275561594193"
+        className="clickable"
+        scale="1.13 1 0.04"
+        events={{ click: () => changeScene("SceneTwo") }}
+      >
+        <Text
+          value="Check In"
+          align="center"
+          position="0 0 0.01"
+          color="#000000"
+        />
+      </Entity>
 
-			{/* <Button
+      {/* <Button
 				text='Ir a Scene 2'
 				x='-1.5'
 				y='1'
@@ -28,6 +33,6 @@ export default function SceneOne({visible, changeScene}) {
 				className='scene2-button'
 				onClick={() => changeScene('SceneTwo')}
 			/> */}
-		</Entity>
-	);
+    </Entity>
+  );
 }
--- src/components/SceneTwo.js
+++ src/components/SceneTwo.js
@@ -1,25 +1,100 @@
 // SceneTwo.js
-import React from 'react';
-import {Sky, Text, Entity} from 'react-aframe-ar';
-import Button from './Button';
+import React from "react";
+import { Sky, Text, Entity } from "react-aframe-ar";
+import Button from "./Button";
 
-export default function SceneTwo({visible, changeScene}) {
-	return (
-		<Entity id='SceneTwo' visible={visible}>
-			<Sky src='/img/checkIn.jpg' rotation='0 -130 0' />
+export default function SceneTwo({ visible, changeScene }) {
+  return (
+    <Entity id="SceneTwo" visible={visible}>
+      {/* <Sky src="/img/salida9.jpg" rotation="0 -130 0" /> */}
+      <Sky src="/img/Oratorio.jpg" rotation="0 -130 0" />
 
-			<Entity
-				geometry='primitive: plane; width: 1; height: 0.5;'
-				material='color: #FFFFFF;'
-				position='-3.86 1.53 -2.53'
-				rotation='-8.7 41.23 3.04'
-				className='clickable'
-				events={{click: () => changeScene('SceneOne')}}
-			>
-				<Text value='Entrada' align='center' position='0 0 0.01' color='#7BC8A4' />
-			</Entity>
+      {/* Oratorio (escena 4)
+      <Entity
+        geometry="primitive: plane; width: 1; height: 0.5;"
+        material="color: #FFFFFF;"
+        position="4.47716 0.54748 -4.84135"
+        rotation="-6.8721 -35.66893 1.76395"
+        className="clickable"
+        events={{ click: () => changeScene("SceneOne") }}
+      >
+        <Text
+          value="CheckIn"
+          align="center"
+          position="0 0 0.01"
+          color="#7BC8A4"
+        />
+      </Entity>
+		*/}
+      {/* CheckIn (escena 3)
+      <Entity
+        geometry="primitive: plane; width: 1; height: 0.5;"
+        material="color: #FFFFFF;"
+        position="-4.73 2.07074 7.98732"
+        rotation="-8.46979 138.94648 0.54011"
+        className="clickable"
+        events={{ click: () => changeScene("SceneOne") }}
+      >
+        <Text
+          value="Oratorio"
+          align="center"
+          position="0 0 0.01"
+          color="#7BC8A4"
+        />
+      </Entity>
 
-			{/* <Button
+      <Entity
+        geometry="primitive: plane; width: 1; height: 0.5;"
+        material="color: #FFFFFF;"
+        position="-5.57691 0.54748 -4.84135"
+        rotation="5.527896807422183 38.19107479224016 2.0139466498848435"
+        className="clickable"
+        events={{ click: () => changeScene("SceneOne") }}
+      >
+        <Text
+          value="Salida 9"
+          align="center"
+          position="0 0 0.01"
+          color="#7BC8A4"
+        />
+      </Entity>
+	  */}
+
+      {/* Salida nueve (escena 2) 
+      <Entity
+        geometry="primitive: plane; width: 1; height: 0.5;"
+        material="color: #FFFFFF;"
+        position="-9.26 2.29074 0.60646"
+        rotation="-8.69979116126642 81.8023303264179 3.040114060964149"
+        className="clickable"
+        events={{ click: () => changeScene("SceneOne") }}
+      >
+        <Text
+          value="Entrada"
+          align="center"
+          position="0 0 0.01"
+          color="#7BC8A4"
+        />
+      </Entity>
+
+      <Entity
+        geometry="primitive: plane; width: 1; height: 0.5;"
+        material="color: #FFFFFF;"
+        position="4.35635 2.63472 1.1059"
+        rotation="5.148598747045577 -91.25269619930569 -2.9117715148548435"
+        className="clickable"
+        events={{ click: () => changeScene("SceneOne") }}
+      >
+        <Text
+          value="Calle"
+          align="center"
+          position="0 0 0.01"
+          color="#7BC8A4"
+        />
+      </Entity>
+		*/}
+
+      {/* <Button
 				text='Ir a Scene 1'
 				x='-1.5'
 				y='1'
@@ -27,6 +102,6 @@ export default function SceneTwo({visible, changeScene}) {
 				className='scene1-button'
 				onClick={() => changeScene('SceneOne')}
 			/> */}
-		</Entity>
-	);
+    </Entity>
+  );
 }
