4d45501ef306956b7e82c6d0e7fd67260d105fa4
Kosthon <alejofova@gmail.com>
2024-03-20 13:04:24 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import React, {useState} from 'react';
+import 'aframe';
+import React from 'react';
 import {Sky, Text, Scene, Entity, Cursor, Camera} from 'react-aframe-ar';
 
 function App() {
-	const [currentScene, setCurrentScene] = useState('main');
-
-	const handleButtonClick = () => {
-		setCurrentScene('new');
-	};
-
 	return (
 		<Scene>
-			{currentScene === 'main' && (
-				<Sky
-					src='https://weloobe.github.io/vr360expo/static_assets/chess-world.jpg'
-					rotation='0 -130 0'
-				/>
-			)}
-			{currentScene === 'new' && <Sky src='URL_DEL_NUEVO_SKY' rotation='0 -130 0' />}
+			<Sky src='/img/entrance.jpg' rotation='0 -130 0' />
 
 			<Entity
 				geometry='primitive: plane; width: 1; height: 0.5;'
 				material='color: #4CC3D9;'
 				position='0 1 -3'
 				className='clickable'
-				events={{click: handleButtonClick}}
+				events={{click: () => console.log('Botón clickeado!')}}
 			>
 				<Text value='Segunda escena' align='center' position='0 0 0.01' color='#7BC8A4' />
 			</Entity>
