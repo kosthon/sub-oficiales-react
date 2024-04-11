a7aba9329fb9ed700d037f69e99aa458995cfeda
kosthon <alejofova@gmail.com>
2024-04-11 13:56:17 -0500

    Add video modal and interactable video plane in Sc

--- src/components/SceneOne.js
+++ src/components/SceneOne.js
@@ -4,21 +4,29 @@ import Modal from './Modal';
 // import Plant from './ModelsComponents/Plant';
 import Gun from './ModelsComponents/Gun'
 import { Canvas } from '@react-three/fiber';
-import { Sky, Entity, Image } from 'react-aframe-ar';
+import { Sky, Entity, Image, Text } from 'react-aframe-ar';
 import { OrbitControls, Environment } from '@react-three/drei';
 
 import './styles/Models.css';
 
 export default function SceneOne({ visible, changeScene }) {
 	const [modalOpen, setModalOpen] = useState(false);
+	const [modalOpenVideo, setModalOpenVideo] = useState(false);
 
 	const handleOpenModal = () => {
-		console.log('CLIKC!!');
 		setModalOpen(true);
 	};
 
 	const handleCloseModal = () => {
 		setModalOpen(false);
+	};
+
+	const handleOpenModalVideo = () => {
+		setModalOpenVideo(true);
+	};
+
+	const handleCloseModalVideo = () => {
+		setModalOpenVideo(false);
 	};
 
 	const handleSceneChange = () => {
@@ -55,6 +63,60 @@ export default function SceneOne({ visible, changeScene }) {
 			</Entity>
 
 			<Entity
+				geometry='primitive: plane; width: 1; height: 1;'
+				material='transparent: true; visible: false'
+				position='-3 2.05 2.4'
+				rotation='0 100 0'
+				scale='0.93 0.87 0.26'
+			>
+				<Text
+					value='MODELO'
+					align='center'
+					position='0 0 0.01'
+					color='#000000'
+				/>
+			</Entity>
+
+			<Entity
+				geometry='primitive: plane; width: 0.5; height: 0.5;'
+				material='transparent: true; visible: false'
+				position='-3 1.6 2.4'
+				rotation='0 100 0'
+				className={visible ? 'clickable' : ''}
+				scale='1.13 1 0.04'
+				events={{
+					mouseenter: e => {
+						e.target.setAttribute('scale', { x: 1.2, y: 1.2, z: 1.2 });
+						e.target.setAttribute(
+							'animation__mouseenter',
+							'property: scale; to: 1.5 1.5 1.5; dur: 350; easing: linear;'
+						);
+					},
+					mouseleave: e => {
+						e.target.setAttribute('scale', { x: 1, y: 1, z: 1 });
+						e.target.removeAttribute('animation__mouseenter');
+					},
+					click: handleOpenModalVideo,
+				}}
+			>
+				<Image src='/img/Options.png' width='0.5' height='0.5' position='0 0 0' />
+			</Entity>
+			<Entity
+				geometry='primitive: plane; width: 1; height: 1;'
+				material='transparent: true; visible: false'
+				position='-3 1.25 2.4'
+				rotation='0 100 0'
+				scale='0.93 0.87 0.26'
+			>
+				<Text
+					value='VIDEO'
+					align='center'
+					position='0 0 0.01'
+					color='#000000'
+				/>
+			</Entity>
+
+			<Entity
 				geometry='primitive: plane; width: 0.5; height: 0.5;'
 				material='transparent: true; visible: false'
 				position='-4 0.2 -3'
@@ -78,6 +140,13 @@ export default function SceneOne({ visible, changeScene }) {
 				<Image src='/img/ArrowYw.png' width='0.5' height='0.5' position='0 0 0' />
 			</Entity>
 
+			<Entity
+				gltf-model='/models/person/scene.gltf'
+				position='-7 0 1'
+				scale='2 2 2'
+				animation__postition='property: rotation; to: 0 3600 0; dir: alternate; dur: 50000; loop: true;'
+			/>
+
 			{modalOpen && (
 				<Modal onClose={handleCloseModal}>
 					<Canvas>
@@ -88,6 +157,12 @@ export default function SceneOne({ visible, changeScene }) {
 						</Suspense>
 						<Environment preset='sunset' />
 					</Canvas>
+				</Modal>
+			)}
+
+			{modalOpenVideo && (
+				<Modal onClose={handleCloseModalVideo}>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/mWADuvOqbDk?si=g8FEAFHsIjllVye6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 				</Modal>
 			)}
 		</Entity>
